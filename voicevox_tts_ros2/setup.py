ccf37e327ed1404505af81e27a4bf840fdcd5312
Haruki Isono <haruki.isono861@gmail.com>
2026-08-21 21:19:19 +0900

    chore

--- voicevox_tts_ros2/setup.py
+++ voicevox_tts_ros2/setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ("share/" + package_name, ["../pyproject.toml"]),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=[
